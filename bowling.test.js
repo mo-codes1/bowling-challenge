182f71fd8d821f8709593c78a60353349eb3a9b1
Mohammed Salah <mohammed_salah@icloud.com>
2021-11-28 13:10:56 +0000

    setup test for a game of all ones

--- bowling.test.js
+++ bowling.test.js
@@ -5,6 +5,17 @@ beforeEach(() => {
 });
 
 it('should return a gutter game', () => {
+  rollIndex(20, 0);
   expect(game.score).toEqual(0);
-
 });
+
+it("should return a game of all ones", () => {
+  rollIndex(20, 1);
+  expect(game.score).toEqual(20);
+});
+
+function rollIndex(rolls, pins){
+  for(let i=0;i<rolls;i++){
+    game.roll(pins);
+  }
+}
